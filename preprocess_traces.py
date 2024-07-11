4c01420ee8b74800783e61fdc0afbadc507bca20
Michael Wrana <michaelwrana@Michaels-MBP-2.lan>
2024-07-11 09:09:22 -0400

    fix pickle dump

--- preprocess_traces.py
+++ preprocess_traces.py
@@ -8,27 +8,25 @@ with open(train_loc, 'rb') as f:
 
 print("Processing Traces (train)")
 train_pos = process_traces(traces_train, "p")
-train_neg = process_traces(traces_train, "n")
-
 with open(train_loc + '_pos', 'wb') as f:
-    pickle.dump(train_pos)
+    pickle.dump(train_pos, f)
 
+train_neg = process_traces(traces_train, "n")
 with open(train_loc + '_neg', 'wb') as f:
-    pickle.dump(train_neg)
+    pickle.dump(train_neg, f)
 
 test_loc  = '../merged_test'
 with open(test_loc, 'rb') as f:
     traces_test = pickle.load(f)
 
 print("Processing Traces (test)")
-test_pos = process_traces(traces_test, "p")
-test_neg = process_traces(traces_test, "n")
-    
+test_pos = process_traces(traces_test, "p") 
 with open(test_loc + '_pos', 'wb') as f:
-    pickle.dump(test_pos)
+    pickle.dump(test_pos, f)
 
+test_neg = process_traces(traces_test, "n")
 with open(test_loc + '_neg', 'wb') as f:
-    pickle.dump(test_neg)
+    pickle.dump(test_neg, f)
 
 print(multiprocessing.cpu_count())
 make_results_folder()
