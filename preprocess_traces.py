e0c912987b3436aa55bafda5284ead8106f91ab8
Michael Wrana <michaelwrana@Michaels-MBP-2.lan>
2024-07-15 01:32:54 -0400

    fix folder gen

--- preprocess_traces.py
+++ preprocess_traces.py
@@ -2,14 +2,14 @@ import pickle
 from pipelinetools import *
 import multiprocessing
 
-train_loc  = '../merged_train'
-with open(train_loc, 'rb') as f:
-    traces_train = pickle.load(f)
+# train_loc  = '../merged_train'
+# with open(train_loc, 'rb') as f:
+#     traces_train = pickle.load(f)
 
-print("Processing Traces (train)")
-train_pos = process_traces(traces_train, "p")
-with open(train_loc + '_pos', 'wb') as f:
-    pickle.dump(train_pos, f)
+# print("Processing Traces (train)")
+# train_pos = process_traces(traces_train, "p")
+# with open(train_loc + '_pos', 'wb') as f:
+#     pickle.dump(train_pos, f)
 
 # train_neg = process_traces(traces_train, "n")
 # with open(train_loc + '_neg', 'wb') as f:
@@ -28,5 +28,5 @@ with open(train_loc + '_pos', 'wb') as f:
 # with open(test_loc + '_neg', 'wb') as f:
 #     pickle.dump(test_neg, f)
 
-print(multiprocessing.cpu_count())
+#print(multiprocessing.cpu_count())
 make_results_folder()
